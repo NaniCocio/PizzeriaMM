44ab6066306c47323eedceafc2eb1bf7ed03998b
Daniela Cocio <daniela.cocio@gmail.com>
2025-03-04 16:46:36 -0300

    actualización menú const - token

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,31 @@ import Button from 'react-bootstrap/Button';
 
 const Navegacion =() => {
     const valor = 25000;
-    const total = valor.toLocaleString()
     const token = false;
 
 return (
     <>
         <Navbar collapseOnSelect className='navbar' expand="lg" bg='dark' data-bs-theme='dark'>
-            <Container>
+            {token ? (
+                <Container>
+                <Navbar.Brand href="#home">Pizzería Mamma Mía</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Button variant="outline-light" className="float-start link">🍕 Home</Button>
+                    <Button variant="outline-light" className="float-start link">🔓 Profile</Button>
+                    <Button variant="outline-light" className="float-start link">🔒 Logout</Button>
+                </Nav>
+                    <Button variant="outline-info" className="float-end">🛒 Total: $ {valor.toLocaleString()}</Button>
+                <Nav>
+                    <Nav.Link href="#d"> </Nav.Link>
+                    
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+
+            ) :(
+                <Container>
                 <Navbar.Brand href="#home">Pizzería Mamma Mía</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
@@ -20,16 +38,16 @@ return (
                     <Button variant="outline-light" className="float-start link">🍕 Home</Button>
                     <Button variant="outline-light" className="float-start link">🔐 Login</Button>
                     <Button variant="outline-light" className="float-start link">🔐 Register</Button>
-                    <Button variant="outline-light" className="float-start link">🔓 Profile</Button>
-                    <Button variant="outline-light" className="float-start link">🔒 Logout</Button>
                 </Nav>
-                    <Button variant="outline-info" className="float-end">🛒 Total: $ {total}</Button>
+                    <Button variant="outline-info" className="float-end">🛒 Total: $ {valor.toLocaleString()}</Button>
                 <Nav>
                     <Nav.Link href="#d"> </Nav.Link>
                     
                 </Nav>
                 </Navbar.Collapse>
             </Container>
+            )}
+            
         </Navbar>
     </>
     
